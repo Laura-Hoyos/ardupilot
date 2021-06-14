36fef63748bb31429ac82cd5387754ebb27d7436
Laura-Hoyos <lauritahoye@gmail.com>
2021-06-14 17:38:07 +0200

    Update mode.h pour inclure Custom_Loiter

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -36,6 +36,7 @@ public:
         ZIGZAG    =    24,  // ZIGZAG mode is able to fly in a zigzag manner with predefined point A and point B
         SYSTEMID  =    25,  // System ID mode produces automated system identification signals in the controllers
         AUTOROTATE =   26,  // Autonomous autorotation
+        CUSTOM_Loiter = 27, // Custom Loiter mode using optical flow and Range Finder to aproach to ground without landing
     };
 
     // constructor
@@ -1685,3 +1686,46 @@ private:
 
 };
 #endif
+
+class ModeCustomLoiter : public Mode {
+
+public:
+    // inherit constructor
+    using Mode::Mode;
+    Number mode_number() const override { return Number::CUSTOM_LOITER; }
+
+    bool init(bool ignore_checks) override;
+    void run() override;
+
+    bool requires_GPS() const override { return true; }
+    bool has_manual_throttle() const override { return false; }
+    bool allows_arming(AP_Arming::Method method) const override { return true; };
+    bool is_autopilot() const override { return false; }
+    bool has_user_takeoff(bool must_navigate) const override { return true; }
+    bool allows_autotune() const override { return true; }
+
+#if PRECISION_LANDING == ENABLED
+    void set_precision_loiter_enabled(bool value) { _precision_loiter_enabled = value; }
+#endif
+
+protected:
+
+    const char *name() const override { return "CUSTOM_LOITER"; }
+    const char *name4() const override { return "CSTLOIT"; }
+
+    uint32_t wp_distance() const override;
+    int32_t wp_bearing() const override;
+
+#if PRECISION_LANDING == ENABLED
+    bool do_precision_loiter();
+    void precision_loiter_xy();
+#endif
+
+private:
+
+#if PRECISION_LANDING == ENABLED
+    bool _precision_loiter_enabled;
+#endif
+
+};
+
